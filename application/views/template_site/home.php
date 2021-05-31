6955deb7742ccba93c818d0aed51d02934b0de39
ivan paulino <ivan.pereira@ifsuldeminas.edu.br>
2021-05-31 14:28:43 -0300

    Correção do dropdown serviços

--- application/views/template_site/home.php
+++ application/views/template_site/home.php
@@ -104,13 +104,16 @@
                     <a class="nav-link" href="<?= site_url("home/tabelas") ?>">Tabelas</a>
                 </li>
                 <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Serviços</a>
-                    <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                    <a class="dropdown-item" href="#">Ação</a>
-                    <a class="dropdown-item" href="#">Outra ação</a>
-                    <a class="dropdown-item" href="#">Algo mais aqui</a>
-                    </div>
-                </li>
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        Serviços
+                    </a>
+                    <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <li><a class="dropdown-item" href="#">Action</a></li>
+                        <li><a class="dropdown-item" href="#">Another action</a></li>
+                        <li><hr class="dropdown-divider"></li>
+                        <li><a class="dropdown-item" href="#">Something else here</a></li>
+                    </ul>
+                    </li>
                 <li class="nav-item">
                     <a class="nav-link" href="<?= site_url("home/login") ?>">Login</a>
                 </li>
